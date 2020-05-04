90bfeaec0a5e3699a003a595c05d9367b5e9e8b9
mgokh <m.gokhan.yildiz@gmail.com>
2020-05-04 23:50:25 +0300

    foreground service eklendi

--- app/src/main/java/com/mgkhnyldz/activityrecognationexample/BackgroundDetectedActivitiesService.java
+++ app/src/main/java/com/mgkhnyldz/activityrecognationexample/BackgroundDetectedActivitiesService.java
@@ -1,16 +1,21 @@
 package com.mgkhnyldz.activityrecognationexample;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Binder;
 import android.os.IBinder;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.ActivityRecognitionClient;
@@ -36,6 +41,12 @@ public class BackgroundDetectedActivitiesService extends Service {
     private ActivityRecognitionClient mActivityRecognitionClient;
 
     IBinder mBinder = new BackgroundDetectedActivitiesService.LocalBinder();
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(getApplicationContext(), "Service restarted",Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
 
     public class LocalBinder extends Binder {
         public BackgroundDetectedActivitiesService getServerInstance() {
@@ -103,6 +114,26 @@ public class BackgroundDetectedActivitiesService extends Service {
         mIntentService = new Intent(this, ActivityDetect.class);
         mPendingIntent = PendingIntent.getService(this, 1, mIntentService, PendingIntent.FLAG_UPDATE_CURRENT);
         requestActivityUpdatesButtonHandler();
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("my_service", "My Background Service", NotificationManager.IMPORTANCE_NONE);
+            channel.setLightColor(Color.BLUE);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (manager != null) {
+                manager.createNotificationChannel(channel);
+            }
+
+        }
+
+        Notification notification = new NotificationCompat.Builder(this, "my_service")
+                .setContentTitle("Foreground Service")
+                .setContentText("Yapılıyor")
+                .setContentIntent(mPendingIntent)
+                .build();
+
+        startForeground(1, notification);
+
     }
 
     public void requestActivityUpdatesButtonHandler() {
